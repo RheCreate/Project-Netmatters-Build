b963a33aef60cc52c9246dfe7d91c05592c12390
RheCreate <75132698+RheCreate@users.noreply.github.com>
2021-05-14 14:30:01 -0500

    update

--- JS/my-sidenav.js
+++ JS/my-sidenav.js
@@ -1,13 +1,13 @@
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("mySidenav").style.width = "333px";
+    document.getElementById("main").style.marginRight = "333px";
   }
   
   /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft = "0";
+    document.getElementById("main").style.marginRight = "0";
   }
 
   
