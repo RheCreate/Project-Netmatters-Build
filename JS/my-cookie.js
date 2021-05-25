61fb26e79060264a62226eae5a62908950753910
RheCreate <75132698+RheCreate@users.noreply.github.com>
2021-05-25 11:23:51 -0500

    Update

--- JS/my-cookie.js
+++ JS/my-cookie.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
     setTimeout(function () {
         $("#cookieConsent").fadeIn(200);
      }, 2000);
-    $("#closeCookieConsent, .cookieConsentOK").click(function() {
+    $(".cookieConsentOK").click(function() {
         $("#cookieConsent").fadeOut(200);
     }); 
 }); 
