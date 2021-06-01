8cbf8fdc7e0cc310132df248e2ad6388f8cc16bf
RheCreate <75132698+RheCreate@users.noreply.github.com>
2021-06-01 10:13:47 -0500

    Update my-sidenav.js

--- JS/my-sidenav.js
+++ JS/my-sidenav.js
@@ -1,11 +1,9 @@
 
-/* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
   document.getElementById("mySidenav").style.width = "333px";
   document.getElementById("bodymain").style.marginRight = "333px";
 }
 
-/* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("bodymain").style.marginRight = "0";
